5f349d8c9b2b5fee7be4abfb61448513fd4bb928
Jarkendar <jarek.skrzypczak@o2.pl>
2021-02-14 19:56:41 +0100

    Small refactor

--- app/src/main/java/com/dinnerplanner/MainActivity.kt
+++ app/src/main/java/com/dinnerplanner/MainActivity.kt
@@ -4,14 +4,10 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.dinnerplanner.ui.dashboard.DashboardFragment
-import com.dinnerplanner.ui.recipeList.RecipeListFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -38,35 +34,8 @@ class MainActivity : AppCompatActivity() {
 
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-//        navView.menu.findItem(R.id.navigation_dashboard).setChecked(true)
-
-        navController.currentDestination?.id
-        //todo to change it, it is very simple implementation
-        var fragment = Fragment()
-        navView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.navigation_home -> {
-                    fragment = RecipeListFragment()
-                }
-                R.id.navigation_dashboard -> {
-                    fragment = DashboardFragment()
-                }
-            }
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.nav_host_fragment, fragment)
-                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                .commit()
-            true
-        }
-
-        navView.selectedItemId = R.id.navigation_home//R.id.navigation_dashboard
-//        findViewById<View>(R.id.navigation_dashboard).performClick()
-//        navView.setOnNavigationItemSelectedListener {
-//            Log.d("********", it.toString())
-//            return@setOnNavigationItemSelectedListener true
-//        }
     }
+
 
     public fun addRecipeToList(view: View) {
         //todo open view to add recipe, remove toast
